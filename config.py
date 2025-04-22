559b306e9493cae28f51a02853a97f169f9c91d8
RaquelCasadoR <raquelcr2003@gmail.com>
2025-04-22 18:27:02 +0200

    predicción a 7 días en test_characteristics

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 symbols = ["BTCUSDT",  "ETHUSDT", "XRPUSDT", "BNBUSDT", "SOLUSDT", "ADAUSDT", "TRXUSDT",
            "LINKUSDT",  "AVAXUSDT"]
 #"ETHBTC", "XRPBTC", "BNBBTC", "SOLBTC", "ADABTC", "TRXBTC", "LINKBTC", "TONBTC", "AVAXBTC", "TONUSDT",
-frequencies = ["1h", "4h", "1d"]
-# 
+frequencies = ["1d"]
+# "1h", "4h",
